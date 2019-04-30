0f0dbb07e3dd47227749eb8a953810d93237ef3b
Jonathan Stites <mail@jonstites.com>
2019-04-30 07:22:24 -0500

    Initial jlox commit

---
+++ src/main/Lox.java
@@ -0,0 +1,11 @@
+package com.craftinginterpreters.lox;
+
+import java.io.IOException;                                  
+
+public class Lox {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Usage: jlox [script]");            
+        
+    }
+}
