9d0760d2cd5bb5d9c7308dc64a679798aba6e0e8
Jonathan Stites <mail@jonstites.com>
2019-05-01 07:47:45 -0500

    Set up gradle correctly

---
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,14 @@
+package com.craftinginterpreters.lox;
+
+import java.io.IOException;
+
+public class Lox {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+        } else {
+            System.out.println("not implemented");
+        }
+    }
+}
