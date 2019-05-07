c9bd54382523945cd74e0785c6cd2f5c1696a1c5
Jonathan Stites <mail@jonstites.com>
2019-05-07 07:20:41 -0500

    Add gitignore and add more to gradle

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -1,14 +1,50 @@
 package com.craftinginterpreters.lox;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.stream.Stream;
+import java.nio.file.Path;
 
 public class Lox {
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
         } else {
-            System.out.println("not implemented");
+            runPrompt();
         }
+    }
+
+    private static void runFile(String path) throws IOException {
+        Path filePath = Paths.get(path);
+        byte[] fileContents = Files.readAllBytes(filePath);
+        String source = new String(fileContents, Charset.defaultCharset());
+        run(source);
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;) {
+            System.out.print("> ");
+            run(reader.readLine());            
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        Stream<String> tokens = scanner.tokens();
+
+        tokens.forEachOrdered(token -> System.out.println(token));
+        scanner.close();
     }
 }
