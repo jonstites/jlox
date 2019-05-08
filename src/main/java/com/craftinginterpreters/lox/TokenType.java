af234a08d32e0a3b1396e906f9d4e5b2253ceba4
Jonathan Stites <mail@jonstites.com>
2019-05-08 06:46:44 -0500

    Partial implementation of scanning

---
+++ src/main/java/com/craftinginterpreters/lox/TokenType.java
@@ -0,0 +1,9 @@
+package com.craftinginterpreters.lox;
+
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR, 
+
+    EOF
+}
